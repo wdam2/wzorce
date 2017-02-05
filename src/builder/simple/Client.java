09eca83a7d332c74f45dc41562ce46e19ab9c09b
wdam2 <mateuszsalasa2@gmail.com>
2017-02-05 20:18:33 +0100

    another commit

--- src/builder/simple/Client.java
+++ src/builder/simple/Client.java
@@ -4,4 +4,17 @@ package builder.simple;
  * Created by mateusz on 2017-02-05.
  */
 public class Client {
+
+
+    public static void main(String[] args) {
+
+        Cat cat = new Cat.Builder("Pusheen")
+                .age(3)
+                .color("Black")
+                .owner("Matt")
+                .build();
+        System.out.println(cat);
+
+    }
+
 }
