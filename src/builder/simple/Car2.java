f27064feb7576e3855ff2e9adf0aa40f1613003a
wdam2 <mateuszsalasa2@gmail.com>
2017-02-05 20:37:19 +0100

    Added class Car2 body

--- src/builder/simple/Car2.java
+++ src/builder/simple/Car2.java
@@ -4,4 +4,28 @@ package builder.simple;
  * Created by mateusz on 2017-02-05.
  */
 public class Car2 {
+
+    public String color;
+
+    private Car2(Builder builder) {
+        this.color = builder.color;
+    }
+
+    public static class Builder {
+
+        private String color;
+
+        public Builder() {
+        }
+
+        public Car.Builder color(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+
+    }
 }
